e16f697efb55cb89decd4cec6bd2895887ef07fe
gards <sgardn04@gmail.com>
2015-03-27 21:05:33 -0400

    allow for the skipping of users, random order

--- public/js/GameCtrl.js
+++ public/js/GameCtrl.js
@@ -1,83 +1,99 @@
 angular.module('GameCtrl', []).controller('GameCtrl', function($scope, $timeout) {
 
-	$scope.people = [
-						{ name : "Neo", pictureUrl : "images/neo.gif", complete : false },
-						{ name : "Trinity", pictureUrl : "images/trinity.gif", complete : false },
-						{ name : "Cypher", pictureUrl : "images/cypher.gif", complete : false },
-						{ name : "Morpheus", pictureUrl : "images/morpheus.gif", complete : false },
-						{ name : "Agent Smith", pictureUrl : "images/agent\ smith.gif", complete : false },
-					];
+  $scope.people = [
+            { name : "Neo", pictureUrl : "images/neo.gif", complete : false },
+            { name : "Trinity", pictureUrl : "images/trinity.gif", complete : false },
+            { name : "Cypher", pictureUrl : "images/cypher.gif", complete : false },
+            { name : "Morpheus", pictureUrl : "images/morpheus.gif", complete : false },
+            { name : "Agent Smith", pictureUrl : "images/agent\ smith.gif", complete : false },
+          ];
 
-	// for two named people...
-	// $scope.people = [
-	// 					{ firstName : "Thomas", lastName : "Anderson", pictureUrl : "images/neo.gif", complete : false },
-	// 					{ firstName : "The", lastName : "Trinity", pictureUrl : "images/trinity.gif", complete : false },
-	// 					{ firstName : "Mr", lastName : "Cypher", pictureUrl : "images/cypher.gif", complete : false },
-	// 					{ firstName : "Lord", lastName : "Morpheus", pictureUrl : "images/morpheus.gif", complete : false },
-	// 					{ firstName : "Agent" lastName : "Smith", pictureUrl : "images/agent\ smith.gif", complete : false },
-	// 				];
+  // for two named people...
+  // $scope.people = [
+  //          { firstName : "Thomas", lastName : "Anderson", pictureUrl : "images/neo.gif", complete : false },
+  //          { firstName : "The", lastName : "Trinity", pictureUrl : "images/trinity.gif", complete : false },
+  //          { firstName : "Mr", lastName : "Cypher", pictureUrl : "images/cypher.gif", complete : false },
+  //          { firstName : "Lord", lastName : "Morpheus", pictureUrl : "images/morpheus.gif", complete : false },
+  //          { firstName : "Agent" lastName : "Smith", pictureUrl : "images/agent\ smith.gif", complete : false },
+  //        ];
 
-	var NUM_PEOPLE = $scope.people.length;
-	$scope.usersLeft = NUM_PEOPLE;
-	$scope.finished = [];
-	$scope.score = 0;
-	$scope.index = Math.floor(Math.random() * NUM_PEOPLE);
-	hint = 0;
+  $scope.finished = [];
+  $scope.score = 0;
+  hint = 0;
 
-	$scope.currentUser = function () {
-		return $scope.people[$scope.index];
-	}
-	
-	function shiftCurrentUser() {
-		var mine = angular.copy($scope.people[$scope.index]);
-		$scope.finished.unshift(mine);
-	}
+  $scope.peopleLeft = angular.copy($scope.people);
 
-	$scope.useHint = function() {
-		if(hint < 3){
-			$scope.score--;
-			hint++;
-			$scope.hint = $scope.currentUser().name.substring(0, hint);
-		} else {
-			flash("no more hints!");
-		}
-	}
+  // ugly, but functional
+  // should this return the index, or update scope.index?
+  function nextIndex () {
+    if($scope.peopleLeft.length){
+      if($scope.peopleLeft.length == 1){
+        flash("last person!");
+        return 0;
+      } else {
+        var newVal = $scope.index;
+        while(newVal == $scope.index){
+          newVal = Math.floor(Math.random() * $scope.peopleLeft.length);
+        }
+        return newVal;
+      }
+    } else {
+      return -1;
+    }
+  }
 
-	// this should skip randomly, which means we should also consume 
-	// our array so we have a single source of truth
-	$scope.skip = function() {
-		$scope.index = ($scope.index + 1) % $scope.usersLeft;
-	}
+  $scope.index = nextIndex();
 
-	// we consume our array in order, to prevent hitting a user we've already seen
-	function nextUser () {
-		shiftCurrentUser();
-		$scope.usersLeft--;
-		if($scope.usersLeft){
-			$scope.people[$scope.index].complete = true;
-			$scope.index = ($scope.index + 1) % NUM_PEOPLE;
-			$scope.name = $scope.hint = "";
-			hint = 0;
-			flash("success");
-		}
-	}
+  $scope.skip = function() {
+    $scope.index = nextIndex();
+  }
 
-	$scope.$watch("name", function (newValue, oldValue) {
-		if(angular.lowercase(newValue) == angular.lowercase($scope.currentUser().name)){
-			$scope.score += 5;
-			nextUser();
-		}
-	});
+  $scope.currentUser = function () {
+    return $scope.peopleLeft[$scope.index];
+  }
+  
+  function shiftCurrentUser() {
+    var mine = $scope.peopleLeft.splice($scope.index, 1);
+    $scope.finished.unshift(mine[0]);
+    $scope.index = nextIndex();
+  }
 
-	flash = function (message) {
-		$scope.message = message;
-		$timeout(function() {
-			$scope.message = "";
-		}, 1000);
-	}
+  $scope.useHint = function() {
+    if(hint < 3){
+      $scope.score--;
+      hint++;
+      $scope.hint = $scope.currentUser().name.substring(0, hint);
+    } else {
+      flash("no more hints!");
+    }
+  }
 
-	// initialize = function () {
-	// }
+  // we consume our array in order, to prevent hitting a user we've already seen
+  function nextUser () {
+    shiftCurrentUser();
+    if($scope.peopleLeft.length){
+      $scope.name = $scope.hint = "";
+      hint = 0;
+      flash("success");
+    }
+  }
 
-	// initialize();
+  $scope.$watch("name", function (newValue, oldValue) {
+    if(angular.lowercase(newValue) == angular.lowercase($scope.currentUser().name)){
+      $scope.score += 5;
+      nextUser();
+    }
+  });
+
+  flash = function (message) {
+    $scope.message = message;
+    $timeout(function() {
+      $scope.message = "";
+    }, 1000);
+  }
+
+  // initialize = function () {
+  // }
+
+  // initialize();
 });
